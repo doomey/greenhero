e3db0f6fdb2817ec86d47887fe3141f702605168
HAEBEOM <haebeompark@naver.com>
2016-03-09 19:19:51 +0900

    1

--- routes/totalexcel.js
+++ routes/totalexcel.js
@@ -7,9 +7,14 @@ var fs = require('fs');
 var mime = require('mime');
 var AWS = require('aws-sdk');
 var s3Config = require('../config/s3Config');
+var bcrypt = require('bcrypt');
+var sqlAes = require('./sqlAES');
+var XLSX = require('xlsx');
+
+sqlAes.setServerKey(serverKey);
 
 router.get('/', function(req, res, next) {
-  if (typeof require !== 'undefined') XLSX = require('xlsx');
+
   var workbook = XLSX.readFile(path.join(__dirname, '../uploads/excel', 'datatable.xlsx'));
   var sheet;
 
@@ -21,6 +26,60 @@ router.get('/', function(req, res, next) {
         callback(null, connection);
       }
     });
+  }
+  
+  
+  function generateSalt(connection, callback){
+    bcrypt.genSalt(10, function(err, salt){
+      if(err){
+        callback(err);
+      } else {
+        callback(null, salt, connection);
+      }
+    })
+  }
+
+  function insertIparty(salt, connection, callback){
+    var sql = "insert into iparty(username, hashpassword, nickname, partytype, name, phone) " +
+        "values(?, ?, ?, ?, " +
+        sqlAes.encrypt(2) +
+        ")";
+
+    async.each(sheet, function(item, cb){
+      //console.log("test : " + item);
+      //console.log("변형되어야 할 값 1: " + item.hashpassword);
+      bcrypt.hash(item.hashpassword, salt, function(err, hashPassword){
+        if(err){
+          callback(err);
+        } else {
+          //console.log("변형되어야 할 값 : " + sheet[i].hashpassword);
+          connection.query(sql, [item.username, hashPassword, item.nickname, item.partytype, item.name, item.phone], function(err, result) {
+            if(err){
+              callback(err);
+            } else {
+              var result = {
+                "id" : result.insertId,
+                "partytype" : item.partytype,
+                "name" : item.name,
+                "phone" : item.phone,
+                "password" : hashPassword
+              }
+              //console.log(result);
+              cb(null, result);
+            }
+          });
+
+        }
+      })
+
+    }, function(err, result){
+      if(err){
+        callback(err);
+      } else {
+        //console.log(result);
+        callback(null, result);
+      }
+    })
   }
 
   function insertBoard(connection, callback) {
@@ -189,10 +248,22 @@ router.get('/', function(req, res, next) {
   }
 
 
+
+
   async.eachSeries(workbook.SheetNames, function (item, callback) {
     var sheet_name = item;
     var worksheet = workbook.Sheets[sheet_name];
     sheet = XLSX.utils.sheet_to_json(worksheet);
+
+    if (sheet_name==="iparty"){
+      async.waterfall([getConnection, generateSalt, insertIparty], function(err, result){
+        if(err){
+          callback(err);
+        } else {
+          callback(null, result);
+        }
+      })
+    }
 
     if (sheet_name === "board") {
       async.waterfall([getConnection, insertBoard], function (err, result) {
