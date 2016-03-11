c273af304860f47bf03772dc94f28551afbec85c
kirsch <kirsch2u@naver.com>
2016-03-11 18:33:39 +0900

    3월 9일 구글로그인 테스트 완료

--- config/googleConfig.js
+++ config/googleConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
     "google" : {
-        "appId" : "639066273635-6d69u53pek31u5n960u611809r7lqufc.apps.googleusercontent.com",
+        "appId" :"639066273635-6d69u53pek31u5n960u611809r7lqufc.apps.googleusercontent.com",
         "appSecret" : "6k_HsEjrik1kwlvUq7zjoYp5"
     }
 }
--- config/passportconfig.js
+++ config/passportconfig.js
@@ -1,15 +1,14 @@
 var async = require('async');
 var bcrypt = require('bcrypt');
 var GoogleConfig = require('./googleConfig');
-var GoogleTokenStrategy = require('passport-google-token').Strategy;
-var LocalStrategy = require('passport-local').Strategy;
+var GoogleTokenStrategy = require('passport-google-id-token');
 var sqlAes = require('../routes/sqlAES.js');
 
 sqlAes.setServerKey(serverKey);
 
-
 module.exports = function(passport) {
     passport.serializeUser(function(user, done) {
+       console.log('유저아이디' , user.id);
         done(null, user.id);
     });
 
@@ -19,7 +18,7 @@ module.exports = function(passport) {
                 done(err);
             } else {
                 var sql = "select " +
-                          "id, username, google_id, google_name, " +
+                          "id, username, google_id, google_name, nickname, " +
                           //"convert(aes_decrypt(name, unhex(" + connection.escape(serverKey) + ")) using utf8) as na, " +
                           //"convert(aes_decrypt(google_email, unhex(" + connection.escape(serverKey) + ")) using utf8) as gemail " +
                           sqlAes.decrypt("name") +
@@ -44,163 +43,92 @@ module.exports = function(passport) {
         });
     });
 
-    //passport.use('google-token', new GoogleTokenStrategy({
-    //    "clientID" : GoogleConfig.google.appId,
-    //    "cliendSecret" : GoogleConfig.google.appSecret
-    //}, function(accessToken, refreshToken, profile, done) {
-    //    console.log('google-token들어옴');
-    //    function getConnection(callback) {
-    //        pool.getConnection(function(err, connection) {
-    //            if(err) {
-    //                callback(err);
-    //            } else {
-    //                callback(null, connection);
-    //            }
-    //        })
-    //    }
-    //
-    //    function selectOrCreateUser(connection) {
-    //        var select = "select id, nickname, google_email, google_name, google_photo "+
-    //                      "from iparty "+
-    //                      "where google_id = ?";
-    //        connection.query(select, [profile.id], function(err, results) {
-    //            if(err) {
-    //                connection.release();
-    //                callback(err);
-    //            } else {
-    //                if(results.length === 0) {
-    //                    var insert = "insert into iparty(nickname, google_id, google_token, google_email, google_name, totalleaf) "+
-    //                                  "values(?, ?, ?, ?, ?, ?)";
-    //                    connection.query(insert, [profile.displayName, profile.id, accessToken, profile.emails[0], profile.displayName, profile.photos[0]], function(err, result) {
-    //                        connection.release();
-    //                        if(err) {
-    //                            callback(err);
-    //                        } else {
-    //                            var user = {
-    //                                "id" : result.insertId,
-    //                                "email" : profile.emails[0],
-    //                                "name" : profile.displayName,
-    //                                "nickname" : profile.displayName
-    //                            }
-    //
-    //                            callback(null, user);
-    //                        }
-    //                    })
-    //                } else {
-    //                    if(accessToken === google_token || profile.emails[0] === results[0].google_email) {
-    //                        connection.release();
-    //                        var user = {
-    //                            "id" : results[0].id,
-    //                            "email" : results[0].email,
-    //                            "name" : results[0].name,
-    //                            "nickname" : results[0].nickname
-    //                        };
-    //                        callback(null, user);
-    //                    } else {
-    //                        var update = "update iparty "+
-    //                                      "set google_token = ?, "+
-    //                                      "    google_email = ?, "+
-    //                                      "    google_name = ? "+
-    //                                      "where google_id = ?"
-    //                        connection.query(update, [accessToken, profile.emails[0], profile.displayName], function(err, result) {
-    //                            connection.release();
-    //                            if(err) {
-    //                                callback(err);
-    //                            } else {
-    //                                var user = {
-    //                                    "id" : results[0].id,
-    //                                    "email" : profile.emails[0],
-    //                                    "name" : profile.displayName,
-    //                                    "nickname" : results[0].nickname
-    //                                };
-    //                                callback(null, user);
-    //                            }
-    //                        });
-    //                    }
-    //                }
-    //            }
-    //        });
-    //    }
-    //
-    //    async.waterfall([getConnection, selectOrCreateUser], function(err, user) {
-    //        if(err) {
-    //            done(err);
-    //        } else {
-    //            done(null, user);
-    //        }
-    //    });
-    //}));
+    passport.use(new GoogleTokenStrategy({
+        "clientID" : GoogleConfig.google.appId,
+        "passReqToCallback" : true
+    }, function(req, parseToken, googleId, done) {
+       console.log('파스토큰', parseToken);
 
-    passport.use('local-login', new LocalStrategy({
-        usernameField: "username",
-        passwordField: "password",
-        passReqToCallback: true
-    }, function(req, username, password, done) {
+       function getConnection(callback) {
+          pool.getConnection(function(err, connection) {
+             if(err) {
+                callback(err);
+             } else {
+                callback(null, connection);
+             }
+          })
+       }
 
-        //1. getConnection
-        function getConnection(callback) {
-            pool.getConnection(function(err, connection) {
-                if(err) {
-                    callback(err);
-                } else {
-                    callback(null, connection);
-                }
-            });
-        }
-        //2. selectpassword
-        function selectIparty(connection, callback) {
-            var select = "select id, username, hashpassword, nickname, google_name, " +
-                         sqlAes.decrypt("google_email", true) +
-                         //"convert(aes_decrypt(google_email, unhex(" + connection.escape(serverKey) + ")) using utf8) as gemail " +
-                         "from iparty " +
-                         "where username = ?";
-            connection.query(select, [username], function(err, results) {
+       function selectOrCreateUser(connection, callback) {
+          var select = "select id, nickname, google_email, google_name "+
+             "from iparty "+
+             "where google_id = ?";
+          connection.query(select, [googleId], function(err, results) {
+             if(err) {
                 connection.release();
-                if(err) {
-                    callback(err);
+                callback(err);
+             } else {
+                if(results.length === 0) {
+                   var insert = "insert into iparty(nickname, google_id, google_token, google_email, google_name, totalleaf, registration_token) "+
+                      "values(?, ?, ?, ?, ?, 0, ?)";
+                   connection.query(insert, [!parseToken.payload.nickname? parseToken.payload.name : parseToken.payload.nickname, googleId, req.body.id_token, parseToken.payload.email, parseToken.payload.name, req.body.registration_id], function(err, result) {
+                      connection.release();
+                      if(err) {
+                         callback(err);
+                      } else {
+                         var user = {
+                            "id" : result.insertId,
+                            "email" : parseToken.payload.email,
+                            "name" : parseToken.payload.name,
+                            "nickname" : parseToken.payload.nickname
+                         }
+
+                         callback(null, user);
+                      }
+                   })
                 } else {
-                    if(results.length === 0) {
-                        var err = new Error('사용자가 존재하지 않습니다...');
-                        callback(err);
-                    } else {
-                        var user = {
-                            "id" : results[0].id,
-                            "hashPassword" : results[0].hashpassword,
-                            "email" : results[0].google_email,
-                            "name" : results[0].google_name,
-                            "nickname" : results[0].nickname
-                        };
-                        console.log('유저', user);
-                        callback(null, user);
-                    }
+                   if(req.body.registration_id === results[0].google_token) {
+                      connection.release();
+                      var user = {
+                         "id" : results[0].id,
+                         "email" : results[0].nickname,
+                         "name" : results[0].nickname,
+                         "nickname" : results[0].nickname
+                      };
+                      console.log('유저', user);
+                      callback(null, user);
+                   } else {
+                      var update = "update iparty "+
+                         "set google_token = ? "+
+                         "where google_id = ?"
+                      connection.query(update, [req.body.id_token, parseToken.payload.email], function(err, result) {
+                         connection.release();
+                         if(err) {
+                            callback(err);
+                         } else {
+                            var user = {
+                               "id" : results[0].id,
+                               "email" : parseToken.payload.email,
+                               "name" : parseToken.payload.name,
+                               "nickname" : results[0].nickname
+                            };
+                            callback(null, user);
+                         }
+                      });
+                   }
                 }
-            });
-        }
+             }
+          });
+       }
 
-        //3. compare
-        function compare(user, callback) {
-            bcrypt.compare(password, user.hashPassword, function(err, result) {
-                if(err) {
-                    console.log('여기 됨');
-                    callback(err);
-                } else {
-                    if(result === true) {
+       async.waterfall([getConnection, selectOrCreateUser], function(err, user) {
+          if(err) {
+             done(err);
+          } else {
+             done(null, user);
+          }
+       });
+    })
+    );
 
-                        callback(null, user);
-                    } else {
-                        callback(null, false);
-                    }
-                }
-            })
-        }
-
-        async.waterfall([getConnection, selectIparty, compare], function(err, user) {
-            if(err) {
-                done(err);
-            } else {
-                delete user.hashPassword;
-                done(null, user);
-            }
-        });
-    }));
 }
