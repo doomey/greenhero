eb06e865ac820f4ec883f92bb9710e0f4c9f9203
HAEBEOM <haebeompark@naver.com>
2016-03-18 14:16:47 +0900

    app ID, Secret 수정

--- config/googleConfig.js
+++ config/googleConfig.js
@@ -1,6 +1,8 @@
 module.exports = {
     "google" : {
-        "appId" :"639066273635-6d69u53pek31u5n960u611809r7lqufc.apps.googleusercontent.com",
-        "appSecret" : "6k_HsEjrik1kwlvUq7zjoYp5"
+        //"appId" :"639066273635-6d69u53pek31u5n960u611809r7lqufc.apps.googleusercontent.com",
+        //"appSecret" : "6k_HsEjrik1kwlvUq7zjoYp5"
+        "appId" : "22478462204-hdj9u2v8jccm0ninv873nts0ibshlkmp.apps.googleusercontent.com",
+        "appSecret" : "-1aJPDDQ_uJ4V1Nb7V-RCz16"
     }
 }
