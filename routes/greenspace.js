93890f75b17b3b7fa7068c6690669e1a63f0df51
HAEBEOM <haebeompark@naver.com>
2016-03-21 10:27:11 +0900

    댓글 관련 console log 추가

--- routes/greenspace.js
+++ routes/greenspace.js
@@ -439,6 +439,7 @@ router.post('/:ediaryId/replies', isLoggedIn, function(req, res, next) {
                 logger.log('debug', '생성된 댓글 내용 : ' + body);
                 logger.log('debug', '생성된 댓글이 속한 게시글 : ' + ediary_id);
                 logger.log('debug', '댓글 작성한 사람 : ' + iparty_id);
+                console.log('댓글ID : ' + replyId + ', 댓글 내용 : ' + body);
                 callback(null, connection);
             }
         });
